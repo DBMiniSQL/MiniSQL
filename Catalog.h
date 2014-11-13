6a8358299967c18eb3355ecf4fc6123407e07f01
MFiveZJU <mfivezju.5@gmail.com>
2014-11-13 22:29:28 +0800

    Update Catalog.h

--- Catalog.h
+++ Catalog.h
@@ -204,7 +204,7 @@ bool catalogManager::Create_Database(string DB_Name)
 
 	return true;
 }
-//emptyAmount/emptyBlock[1000]
+//emptyAmount/emptyBlock[100]:-1。
 bool catalogManager::Create_Table(table_info newTable, string DB_Name)
 {
 	if(DB_Name != dbName)
