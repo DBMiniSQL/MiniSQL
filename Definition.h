6bb0f471a90d59d1acb1c93d086f736a1be521b1
Tbabm <liuzxzju@gmail.com>
2014-11-18 00:05:31 +0800

    modify the definition and buffer

--- Definition.h
+++ Definition.h
@@ -21,7 +21,7 @@ using namespace std;
 
 vector<string> values;
 
-class attr_info
+class AttrInfo
 {
 public:
     string name;
@@ -30,61 +30,94 @@ public:
 	bool isPrimeryKey;
 	bool isUnique;
 	string indexName;
-	attr_info():isPrimeryKey(false),isUnique(false){};
-	attr_info(string a_name, int a_type, int a_length, bool isP, bool isU):name(a_name), type(a_type), length(a_length), isPrimeryKey(isP), isUnique(isU){};
+	AttrInfo():isPrimeryKey(false),isUnique(false){};
+	AttrInfo(string a_name, int a_type, int a_length, bool isP, bool isU):name(a_name), type(a_type), length(a_length), isPrimeryKey(isP), isUnique(isU){};
 };
 
-class table_info
+class TableInfo
 {
 public:
 	string name;
 	int attrNum;	//表中含有的属性数
 	int blockNum;
 	int totalLength;	//值为sum(attributes[i].length)
-	vector<attr_info> attributes;
-	table_info():attrNum(0), blockNum(0), totalLength(0){};
+	vector<AttrInfo> attributes;
+	TableInfo():attrNum(0), blockNum(0), totalLength(0){};
 };
 
-class index_info
+class IndexInfo
 {
 public:
 	string name;
 	string tableName;
 	string attrName;
 	int blockNum;
-	index_info():blockNum(0){};
-	~index_info(){};
+	IndexInfo():blockNum(0){};
+	~IndexInfo(){};
 };
 
-class blockInfo
+class BlockInfo
 {
 public:
 	int blockNum;
 	bool dirtyBit;
-	blockInfo* next;
-	fileInfo* file;
+	BlockInfo* next;
+	FileInfo* file;
 	int charNum;
 	char* cBlock;
 	int iTime;
 	int lock;
 	int father;
 	bool isFull;
-	blockInfo();
-	~blockInfo();
-	void clearBlock();
+	BlockInfo(){
+		blockNum = -1;
+		dirtyBit = 0;
+		next = NULL;
+		file = NULL;
+		charNum = 0;
+		cBlock = new char[BLOCKSIZE];
+		father = -1;
+		iTime = 0;
+		lock = 0;	
+		isFull = 0;
+	}
+	~BlockInfo(){
+		delete [] cBlock;
+	}
+	void clearBlock(){
+		blockNum = -1;
+		dirtyBit = 0;
+		next = NULL;
+		file = NULL;
+		charNum = 0;
+		delete [] cBlock;
+		cBlock = new char[BLOCKSIZE];
+		father = -1;
+		iTime = 0;
+		lock = 0;
+		isFull = 0;
+	}
 };
 
-class fileInfo{
+class FileInfo{
 public:
 	int type;					//0 for Data File
 								//1 for Index File
 	string fileName;			//the name of the file
 	int recordAmount;			//the number of the record in th file
 	int freeNum;				//the free block number which could be used for the file
-	fileInfo* next;				//the pointer points to the next file
-	blockInfo* firstBlock;		//point to the first blcok within the file
-	fileInfo();
-	~fileInfo();
+	FileInfo* next;				//the pointer points to the next file
+	BlockInfo* firstBlock;		//point to the first blcok within the file
+	FileInfo();
+	FileInfo(int fileType,string name){
+		type = fileType;
+		fileName = name;
+		recordAmount = 0;
+		freeNum = 0;
+		next = NULL;
+		firstBlock = NULL;
+	}
+	~FileInfo();
 };
 
 
